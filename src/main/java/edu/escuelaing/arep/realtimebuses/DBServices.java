857f8f471b092e672a5f7e3009cab2f6a403681d
Santiago Vega <svegaromero@gmail.com>
2020-05-20 14:01:03 -0500

    APIGateway

--- src/main/java/edu/escuelaing/arep/realtimebuses/DBServices.java
+++ src/main/java/edu/escuelaing/arep/realtimebuses/DBServices.java
@@ -17,32 +17,30 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DeleteItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+import com.mashape.unirest.http.JsonNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import org.json.JSONObject;
 
 /**
  *
  * @author Santiago
  */
-public class DBServices implements RequestHandler<ApiGatewayRequest, ApiGatewayProxyResponse>  {
+public class DBServices /*implements RequestHandler<ApiGatewayRequest, ApiGatewayProxyResponse>  */{
     
-    @Override
-    public ApiGatewayProxyResponse handleRequest(ApiGatewayRequest i, Context cntxt) {
-        
-        Map<String, String> parameters = i.getQueryStringParameters();
-        
-        String ID = parameters.get("IDBus"); 
-        String lati = parameters.get("Latitud"); 
-        String longi = parameters.get("Longitud"); 
-        
-        double lat = Double.parseDouble(lati);
-        double lon = Double.parseDouble(longi);
-        
+    /*@Override*/
+    public static String handleRequest() {
         
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
             .withRegion(Regions.US_EAST_1)
@@ -50,34 +48,25 @@ public class DBServices implements RequestHandler<ApiGatewayRequest, ApiGatewayP
         DynamoDB dynamoDB = new DynamoDB(client);
         Table table = dynamoDB.getTable("RealTimeBuses");
 
-        try {
-
-            UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("IDBus", ID)
-                .withUpdateExpression("set Latitud= :val1 , Longitud= :val2")
-                .withValueMap(new ValueMap().withNumber(":val1", lat).withNumber(":val2", lon))
-                .withReturnValues(ReturnValue.UPDATED_NEW);
-            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
-
-            // Check the response.
-            System.out.println("Printing item after adding new attribute...");
-            System.out.println(outcome.getItem().toJSONPretty());
-
-        }
-        catch (Exception e) {
-            System.err.println("Failed to add new attribute in " + table);
-            System.err.println(e.getMessage());
-            Item item = new Item().withPrimaryKey("IDBus", 120)
-                    .withNumber("Latitud", lat)
-                    .withNumber("Longitud", lon);
-            table.putItem(item);
-            
-        }
+        ArrayList <String> itemStr= new ArrayList<String>();
         
+        ScanSpec scanSpec = new ScanSpec().withProjectionExpression("IDBus, Latitud, Longitud");
+
+        ItemCollection<ScanOutcome> items = table.scan(null, // FilterExpression
+                "IDBus, Latitud, Longitud", // ProjectionExpression
+                null, // ExpressionAttributeNames - not used in this example
+                null);
+
+            System.out.println("Scan of RealTimeBuses for items ");
+            Iterator<Item> iterator = items.iterator();
+            while (iterator.hasNext()) {
+                
+                String itemObj= iterator.next().toJSON();
+                
+                itemStr.add(itemObj);
+               
+            }
         
-        
-        Map<String, String> headers=i.getHeaders();
-        ApiGatewayProxyResponse response = new ApiGatewayProxyResponse(200,headers,ID+" "+Double.toString(lat)+" "+Double.toString(lon));
-        System.out.println(i.toString());
-        return response;
+        return Arrays.toString(itemStr.toArray());
     }
 }
